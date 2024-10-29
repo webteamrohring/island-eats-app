7cceb0b301f770abcd62f47f32cbdefbcaed1164
jonamonti <jonathanmonti@hotmail.com>
2024-10-29 14:24:45 -0300

    feat: splash screen

--- src/app/views/SplashScreen.jsx
+++ src/app/views/SplashScreen.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import uiColors from '@utils/colors';
 
 const SplashScreen = () => {
-  return (
-    <div>SplashScreen</div>
+  return(
+    <View style={s.container}>
+      <Text>Splash screen</Text>
+    </View>
   );
 };
 
 export default SplashScreen;
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: uiColors.green.normal,
+  },
+});
