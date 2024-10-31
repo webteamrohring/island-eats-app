f2974927340f07566a84e9374a02cae11fd67467
Agustin Garnero <agusgarnero3@gmail.com>
2024-10-31 09:39:33 -0300

    Fix: Some styles incompatibility

--- src/app/views/Login/Login.js
+++ src/app/views/Login/Login.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Dimensions, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import uiColors from '@utils/colors';
 import Logo from '@assets/images/TransparentWhiteLogo.png';
 import Input from '@components/Inputs/Input';
@@ -7,18 +15,29 @@ import Buttons from '@components/Buttons/Buttons';
 import Checkbox from '@components/Checkbox/Checkbox';
 import useMethod from './method';
 
-const { width } = Dimensions.get('screen');
+const {width} = Dimensions.get('screen');
 
 const Login = () => {
-  const { login, handleChange } = useMethod();
+  const {login, handleChange} = useMethod();
 
   return (
     <SafeAreaView style={s.container}>
       <View style={s.innerContainer}>
         <Image source={Logo} />
         <Text style={s.header}>Welcome Rider!</Text>
-        <Input label="Email" value={login.email} fieldName="email" callback={(value) => handleChange(value, 'email')} />
-        <Input label="Password" value={login.password} fieldName={'password'} callback={(value) => handleChange(value, 'password')} isPassword />
+        <Input
+          label="Email"
+          value={login.email}
+          fieldName="email"
+          callback={value => handleChange(value, 'email')}
+        />
+        <Input
+          label="Password"
+          value={login.password}
+          fieldName={'password'}
+          callback={value => handleChange(value, 'password')}
+          isPassword
+        />
         <View style={s.rememberContainer}>
           <Checkbox />
           <Text style={s.text}>Remember me</Text>
@@ -64,8 +83,7 @@ const s = StyleSheet.create({
   },
   text: {
     fontSize: 14,
-    fontWeight: 400,
+    fontWeight: '400',
     color: uiColors.white.normal,
   },
-
 });
